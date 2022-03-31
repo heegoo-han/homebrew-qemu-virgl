d165e5488a2f8d7ba82c4fa4fc0db0527ce0e80c
Heegoo Han <heegoo.han@lge.com>
2022-03-31 14:01:52 +0900

    use the fully-qualified name

--- Formula/libepoxy-angle.rb
+++ Formula/libepoxy-angle.rb
@@ -19,8 +19,8 @@ class LibepoxyAngle < Formula
 
   def install
     mkdir "build" do
-      system "meson", *std_meson_args, "-Dc_args=-I#{Formula["libangle"].opt_prefix}/include",
-             "-Dc_link_args=-L#{Formula["libangle"].opt_prefix}/lib", "-Degl=yes", "-Dx11=false",
+      system "meson", *std_meson_args, "-Dc_args=-I#{Formula["heegoo-han/qemu-virgl/libangle"].opt_prefix}/include",
+             "-Dc_link_args=-L#{Formula["heegoo-han/qemu-virgl/libangle"].opt_prefix}/lib", "-Degl=yes", "-Dx11=false",
              "-Dfallback-libdir=#{HOMEBREW_PREFIX}/lib", ".."
       system "ninja", "-v"
       system "ninja", "install", "-v"
